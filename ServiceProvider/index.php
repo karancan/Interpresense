8ec47590bac9f8be6fbf7ce8b20f2222f3913907
Vincent <vincent.diep61@gmail.com>
2014-03-24 23:00:19 -0400

    New controller structure

--- ServiceProvider/index.php
+++ ServiceProvider/index.php
@@ -1,34 +1,59 @@
 <?php
-//============================================================================================
-// Session, configuration file, localization constructor
-//============================================================================================
-require '../includes/php/config.php';
 
+namespace ServiceProvider;
+
+/**
+ * Session
+ */
 session_start();
 
-//============================================================================================
-// MODEL
-//============================================================================================
+/**
+ * Configuration file
+ */
+require '../includes/php/config.php';
 
-//============================================================================================
-// Load the content
-//============================================================================================
+/**
+ * Models
+ */
+
+/**
+ * Localization
+ */
+require FS_VENDOR . '/JsonI18n.php';
+
+if(!isset($_SESSION['lang'])) {
+    $_SESSION['lang'] = DEFAULT_LANGUAGE;
+}
+\Locale::setDefault($_SESSION['lang']);
+
+// Translation
+$translate = new \JsonI18n(\Locale::getDefault());
+// @todo Add resource files using $translate->addResource($file)
+
+// Date formatting
+// @todo Figure this out
+// Perhaps $dateFmt = new \JsonI18nDate(\Locale::getDefault());
+
+// Number formatting
+// @todo Also figure this out
+
+/**
+ * Content and actions
+ */
 if (!isset($_GET['page'])) {
-
-    $page_title = "index page";
     $viewFile = "views/index.php";
 } else {
     require_once FS_PHP.'/error.php';
 }
 
-//============================================================================================
-// View
-//============================================================================================
+/**
+ * View
+ */
 $actions = array('');
 
 if (!in_array($_GET['page'], $actions, true)) {
 
-    require FS_PHP.'/header.php';
+    require FS_PHP . '/header.php';
     require 'views/nav.php';
 
     if(isset($viewFile) && file_exists($viewFile)) {
@@ -37,6 +62,5 @@ if (!in_array($_GET['page'], $actions, true)) {
         require FS_PHP . '/error.php';
     }
 
-    require_once FS_PHP.'/footer.php';
+    require FS_PHP . '/footer.php';
 }
-?>
