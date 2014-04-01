843e8de04d37c451252f9cf417b4d869dd49f0d2
karancan <karan-91@outlook.com>
2014-04-01 00:17:38 -0400

    Make headers much much prettier

--- Admin/views/header.php
+++ Admin/views/header.php
@@ -2,12 +2,48 @@
     <div class="navbar navbar-static-top">
         <div class="navbar-inner">
             <div class="container">
+                
+                <div class="col-md-1">
+                    
+                    <div class="row">
+                        <img class="org-logo" alt="University of Ottawa" src="../includes/img/logo_icon_379_379.png">
+                    </div>
+                    
+                </div>
+                
+                <div class="col-md-9">
+                    
+                    <div class="row">
+                        <h4 class="org-parent-name">University of Ottawa</h4>
+                        <h2 class="org-name">Access Service</h2>
+                    </div>
+                    
+                </div>
             
-                <div class="col-md-10">
-                    <h2><i class="fa fa-camera-retro"></i> Institution name - Department name</h2>
+                <div class="col-md-2">
+                    
+                    <div class="row">
+                        <div class="btn-group header-options-container pull-right">
+                            <button type="button" class="btn btn-info"><i class="fa fa-gear"></i> Options</button>
+                            <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">
+                                <span class="caret"></span>
+                                <span class="sr-only">Toggle Dropdown</span>
+                            </button>
+                            <ul class="dropdown-menu" role="menu">
+                                <li class="dropdown-header">Language</li>
+                                <li><a href="#">English <i class="fa fa-check"></i></a></li>
+                                <li><a href="#">French</a></li>
+                                <li class="divider"></li>
+                                <li class="dropdown-header">Other</li>
+                                <li><a href="#">Help manual</a></li>
+                                <li><a href="#">Contact department</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    
                 </div>
                 
             </div>
         </div>
     </div>
-</header>    
+</header>
