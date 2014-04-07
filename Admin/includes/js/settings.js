4acd4374e2b8b55ae6d11ca04e0db9b9c8c12423
karancan <karan-91@outlook.com>
2014-04-07 18:24:12 -0400

    More progress

--- Admin/includes/js/settings.js
+++ Admin/includes/js/settings.js
@@ -1,8 +1,30 @@
 /**
+ *User wants to add a setting
+ */
+$('[data-action="add"]').click(function(){
+    
+    $('#admin-add-setting-modal .modal-title').text('Add setting');
+    
+    // @todo: vdiep....add row highlighting
+    
+});
+
+/**
+ *User wants to edit a setting
+ */
+$('[data-action="edit"]').click(function(){
+    
+    $('#admin-add-setting-modal .modal-title').text('Edit setting');
+    
+    // @todo: vdiep....add row highlighting
+    
+});
+
+/**
  *User wants to delete a setting
  */
-$('.btn').click(function(){
-    console.log('ehere');
+$('[data-action="delete"]').click(function(){
+
     // @todo: vdiep....add row highlighting
     
     if(confirm("Are you sure you want to delete this setting? This action is not reversible and could cause the application to malfunction.")){
--- Admin/views/settings.php
+++ Admin/views/settings.php
@@ -8,7 +8,7 @@
             <h3 class="admin-page-title"><i class="fa fa-gears"></i> Settings</h3>
         </div>
         <div class="col-md-2 col-md-offset-1">
-            <a data-toggle="modal" href="#admin-add-setting-modal" class="btn btn-info btn-block admin-add-button"><i class="fa fa-plus"></i> Add a setting</a>        
+            <a data-toggle="modal" href="#admin-add-setting-modal" data-action="add" class="btn btn-info btn-block admin-add-button"><i class="fa fa-plus"></i> Add a setting</a>        
         </div>
     </div>
     
@@ -33,8 +33,8 @@
                              "<td>{$s['setting_key']}</td>" .
                              "<td>{$s['setting_value']}</td>" .
                              '<td class="table-option-cell">
-                                <button type="button" class="btn btn-warning" data-toggle="modal" href="#admin-add-setting-modal"><i class="fa fa-edit"></i> Edit</button>
-                                <button type="button" class="btn btn-danger delete-setting"><i class="fa fa-minus"></i> Delete</button>
+                                <button type="button" class="btn btn-warning" data-toggle="modal" href="#admin-add-setting-modal" data-action="edit"><i class="fa fa-edit"></i> Edit</button>
+                                <button type="button" class="btn btn-danger" data-action="delete"><i class="fa fa-minus"></i> Delete</button>
                               </td>' .
                              '</tr>';
                     }
@@ -97,7 +97,7 @@
             
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">Add a setting</h4>
+                <h4 class="modal-title">Add setting</h4>
             </div>
             
             <div class="modal-body">
