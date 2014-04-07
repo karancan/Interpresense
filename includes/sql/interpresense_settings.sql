825c16dc9c24e31eddecf9dbd1239156df74c958
karancan <karan-91@outlook.com>
2014-04-07 13:28:24 -0400

    Critical issues with SQL file

--- includes/sql/interpresense_settings.sql
+++ includes/sql/interpresense_settings.sql
@@ -10,7 +10,7 @@ CREATE TABLE `interpresense_settings` (
   `setting_key` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `setting_value` text COLLATE utf8_unicode_ci DEFAULT NULL,
   `inserted_on` datetime NOT NULL,
-  `updated_on` datetime NOT NULL
+  `updated_on` datetime NOT NULL,
   PRIMARY KEY (`setting_id`),
   UNIQUE KEY `setting_key_UNIQUE` (`setting_key`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -22,6 +22,6 @@ CREATE TABLE `interpresense_settings` (
 
 LOCK TABLES `interpresense_settings` WRITE;
 /*!40000 ALTER TABLE `interpresense_settings` DISABLE KEYS */;
-INSERT INTO `interpresense_settings` VALUES (1,'invoice_file_attachments_allowed','1','2014-03-24 00:00:00'),(2,'institution_name','University of Ottawa','2014-03-24 00:00:00'),(3,'institution_address','550 Cumberland','2014-03-24 00:00:00'),(4,'institution_email','adapt@uottawa.ca','2014-03-24 00:00:00'),(5,'institution_phone','6135625800','2014-03-24 00:00:00'),(6,'institution_default_lang','en-CA','2014-03-24 00:00:00'),(7,'institution_logo',NULL,'2014-03-24 00:00:00'),(8,'invoicing_earliest_possible_hour','7','2014-03-24 00:00:00'),(9,'invoicing_latest_possible_hour','22','2014-03-24 00:00:00');
+INSERT INTO `interpresense_settings` VALUES (1,'invoice_file_attachments_allowed','1','2014-03-24 00:00:00','2014-03-24 00:00:00'),(2,'institution_name','University of Ottawa','2014-03-24 00:00:00','2014-03-24 00:00:00'),(3,'institution_address','550 Cumberland','2014-03-24 00:00:00','2014-03-24 00:00:00'),(4,'institution_email','adapt@uottawa.ca','2014-03-24 00:00:00','2014-03-24 00:00:00'),(5,'institution_phone','6135625800','2014-03-24 00:00:00','2014-03-24 00:00:00'),(6,'institution_default_lang','en-CA','2014-03-24 00:00:00','2014-03-24 00:00:00'),(7,'institution_logo',NULL,'2014-03-24 00:00:00','2014-03-24 00:00:00'),(8,'invoicing_earliest_possible_hour','7','2014-03-24 00:00:00','2014-03-24 00:00:00'),(9,'invoicing_latest_possible_hour','22','2014-03-24 00:00:00','2014-03-24 00:00:00');
 /*!40000 ALTER TABLE `interpresense_settings` ENABLE KEYS */;
 UNLOCK TABLES;
