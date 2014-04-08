9b351e73a952bfe8a6a176bb27d919dd2fb7ebdc
karancan <karan-91@outlook.com>
2014-04-08 14:08:15 -0400

    Hahaha

--- Admin/views/settings.php
+++ Admin/views/settings.php
@@ -29,7 +29,7 @@
                 <tbody>
                     <?php
                     foreach($appSettings as $s) {
-                        echo "<tr data-setting-id='{$antiXSS->escape($s['setting_key'], 2)}' data-setting-key='{$antiXSS->escape($s['setting_key'], 2)}' data-setting-value='{$antiXSS->escape($s['setting_value'], 2)}'>" .
+                        echo "<tr data-setting-id='{$antiXSS->escape($s['setting_key'], $antiXSS::HTML_ATTR)}' data-setting-key='{$antiXSS->escape($s['setting_key'], $antiXSS::HTML_ATTR)}' data-setting-value='{$antiXSS->escape($s['setting_value'], $antiXSS::HTML_ATTR)}'>" .
                              "<td>{$s['setting_key']}</td>" .
                              "<td>{$s['setting_value']}</td>" .
                              '<td class="table-option-cell">
