b5196c7299bd4c700542462fbf00c5887de56db3
karancan <karan-91@outlook.com>
2014-04-10 21:44:54 -0400

    Footer styling improvements and semantic improvements

--- includes/php/footer.php
+++ includes/php/footer.php
@@ -3,14 +3,23 @@
             <nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
                 
                 <div class="container">
-                    <div class="row">
-                        
-                        <div class="col-md-12">
-                            <img class="logo pull-left" src="../includes/img/logo_regular_1024_350.png" alt="Interpresense">
-                            <p class="navbar-text pull-right"><?= date('Y') ?> &copy; Vincent Diep, Mr. Julian Huff &amp; Karan Khiani</p> 
+                
+                    <div class="row navbar-content">
+            
+                        <div class="col-md-8">
+                            <?= date('Y') ?> &copy; Interpresense by Vincent Diep, Mr. Julian Huff &amp; Karan Khiani
                         </div>
                         
+                        <div class="col-md-2">
+                            <a href="#" class="btn btn-block btn-info"><i class="fa fa-phone"></i> Get in touch</a>
+                        </div>
+                    
+                        <div class="col-md-2">
+                            <a href="mailto:<?= EMAIL_INTERPRESENSE_ERROR_REPORTING ?>?subject=Interpresense%20bug%20report" class="btn btn-block btn-warning"><i class="fa fa-bug"></i> Report a bug</a>
+                        </div>
+                    
                     </div>
+                    
                 </div>
                 
             </nav>
