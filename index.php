914e76df97c27e8c9a17189fda8400cf863026d1
karancan <karan-91@outlook.com>
2014-04-15 22:36:27 -0400

    Major semantic and UI changes to intro page

--- index.php
+++ index.php
@@ -14,14 +14,14 @@ header('Content-Type: text/html; charset=utf-8');
     <link rel='stylesheet' href='//<?= URL_CSS ?>/interpresense.css'>
     
     <style>
-        h3{
-            font-weight: normal;
-            line-height: 1.5em;
-            margin-top: 0;
+        header{
+            background-color: inherit;
         }
         video{
             width: 100%;
-            margin: 1em 0;
+        }
+        .jumbotron, footer{
+            margin: 2em 0;
         }
     </style>
     
@@ -41,42 +41,52 @@ header('Content-Type: text/html; charset=utf-8');
     <body>
         
         <div class="container">
-        
+            
+            <header>
+            
+                <div class="row">
+                    <div class="col-md-8">
+                        <a href="//<?= URL_INTERPRESENSE ?>/">
+                            <img class="external-page-logo" src="//<?= URL_IMAGES ?>/logo_regular_1024_350.png" alt="Interpresense">
+                        </a>
+                    </div>
+                    <div class="col-md-2 col-md-offset-2">
+                        <div class="btn-group header-options-container pull-right">
+                            <button type="button" class="btn btn-info"><i class="fa fa-gear"></i> Language</button>
+                            <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">
+                                <span class="caret"></span>
+                                <span class="sr-only">Toggle Dropdown</span>
+                            </button>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">English <i class="fa fa-check"></i></a></li>
+                                <li><a href="#">French</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            
+            </header>
+            
             <div class="row">
-                
-                <div class="col-md-8">
-                    <a href="//<?= URL_INTERPRESENSE ?>/">
-                        <img class="external-page-logo" src="//<?= URL_IMAGES ?>/logo_regular_1024_350.png" alt="Interpresense">
-                    </a>
+                <div class="col-md-12">
+                    <div class="jumbotron">
+                        <div class="jumbotron-photo">
+                            <video>
+                                <source src="movie.mp4" type="video/mp4">
+                                <source src="movie.ogg" type="video/ogg">
+                            </video>
+                        </div>
+                        <div class="jumbotron-contents">
+                            <h1>Modern and intuitive for <a href="//<?= URL_SERVICE_PROVIDER ?>/">service providers</a>. Powerful and flexible for <a href="//<?= URL_ADMIN ?>/">administrators</a>.</h1>
+                            <h2><a href="//<?= URL_SERVICE_PROVIDER ?>/">Service providers</a></h2>
+                            <p>Interpreters, note-takers and other service providers create, save and send invoices.</p>
+                            <h2><a href="//<?= URL_ADMIN ?>/">Administrators</a></h2>
+                            <p>Administrators receive, approve and respond to invoices and generate reports and statistics in a few clicks.</p>
+                        </div>
+                    </div>
                 </div>
-                
-                <div class="col-md-2">
-                    <a href="//<?= URL_INTERPRESENSE ?>/ServiceProvider/" class="external-page-btn-redirect btn btn-block btn-info"><i class="fa fa-hand-o-right"></i> I am an interpreter</a>
-                </div>
-                
-                <div class="col-md-2">
-                    <a href="//<?= URL_INTERPRESENSE ?>/Admin/" class="external-page-btn-redirect btn btn-block btn-info"><i class="fa fa-hand-o-right"></i> I am an administrator</a>
-                </div>
-                
             </div>
             
-            <hr class="inset">
-            
-            <div class="row">
-                <div class="col-md-12 text-center">
-                    <h3>Modern and intuitive for service providers. Powerful and flexible for administrators.</h3>
-                </div>
-            </div>
-            
-            <div class="row">
-                <div class="col-md-10 col-md-offset-1 text-center">
-                    <video>
-                        <source src="movie.mp4" type="video/mp4">
-                        <source src="movie.ogg" type="video/ogg">
-                    </video>
-                </div>
-            </div>
-
             <hr class="inset">
             
             <footer role='contentinfo'>
