063f12d5cfc459edd831f29e0be5101e0bbc02bc
karancan <karan-91@outlook.com>
2014-04-17 17:51:29 -0400

    Footers made non-sticky

--- includes/php/footer.php
+++ includes/php/footer.php
@@ -1,30 +1,28 @@
-        <footer role='contentinfo'>
+        <div class="container">
             
-            <nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+            <hr class="inset">
+        
+            <footer role='contentinfo'>
                 
-                <div class="container">
+                <div class="row navbar-content">
                 
-                    <div class="row navbar-content">
-            
-                        <div class="col-md-8">
-                            <?= date('Y') ?> &copy; Interpresense by Vincent Diep &amp; Karan Khiani
-                        </div>
-                        
-                        <div class="col-md-2">
-                            <a href="mailto:<?= EMAIL_INTERPRESENSE_REPORTING ?>?subject=Interpresense%20query" class="btn btn-block btn-info"><i class="fa fa-phone"></i> Get in touch</a>
-                        </div>
-                    
-                        <div class="col-md-2">
-                            <a href="mailto:<?= EMAIL_INTERPRESENSE_REPORTING ?>?subject=Interpresense%20bug%20report" class="btn btn-block btn-warning"><i class="fa fa-bug"></i> Report a bug</a>
-                        </div>
-                    
+                    <div class="col-md-8">
+                        <?= date('Y') ?> &copy; Interpresense by Vincent Diep &amp; Karan Khiani
                     </div>
                     
-                </div>
+                    <div class="col-md-2">
+                        <a href="mailto:<?= EMAIL_INTERPRESENSE_REPORTING ?>?subject=Interpresense%20query" class="btn btn-block btn-info"><i class="fa fa-phone"></i> Get in touch</a>
+                    </div>
                 
-            </nav>
+                    <div class="col-md-2">
+                        <a href="mailto:<?= EMAIL_INTERPRESENSE_REPORTING ?>?subject=Interpresense%20bug%20report" class="btn btn-block btn-warning"><i class="fa fa-bug"></i> Report a bug</a>
+                    </div>
+                
+                </div>
             
-        </footer>
+            </footer>
+            
+        </div>
 
     </body>
 
