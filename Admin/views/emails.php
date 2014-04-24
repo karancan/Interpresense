4c3c1dbbe3d24518b46796b062c7df7ff2e77d05
karancan <karan-91@outlook.com>
2014-04-24 13:59:48 -0400

    Adjust view

--- Admin/views/emails.php
+++ Admin/views/emails.php
@@ -20,6 +20,8 @@
                     <tr>
                         <th scope='col'>Name</th>
                         <th scope='col'>Description</th>
+                        <th scope='col'>CC</th>
+                        <th scope='col'>BCC</th>
                         <th scope='col'>Options</th>
                     </tr>
                 </thead>
