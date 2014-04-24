f1760c769cf3454071938999673b3cd034c525ef
karancan <karan-91@outlook.com>
2014-04-24 13:52:05 -0400

    MVLC for emails and changes to nav to link to emails

---
+++ Admin/views/emails.php
@@ -0,0 +1,37 @@
+<style>
+    @import url('includes/css/admin.css');
+</style>
+<div class="container">
+    
+    <div class="row">
+        <div class="col-md-8">
+            <h3 class="admin-page-title"><i class="fa fa-users"></i> Email templates</h3>
+        </div>
+    </div>
+    
+    <div class="row">
+        
+        <div class="col-md-12">
+        
+            <h4>All email templates</h4>
+            
+            <table id="admin-users-table" class="table table-hover invoice-table">           
+                <thead>
+                    <tr>
+                        <th scope='col'>Name</th>
+                        <th scope='col'>Description</th>
+                        <th scope='col'>Options</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <!-- @todo: spit out all emails -->
+                    </tr>
+                </tbody>
+            </table>
+        
+        </div>
+        
+    </div>
+    
+</div>
