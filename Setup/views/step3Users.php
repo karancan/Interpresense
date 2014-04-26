7212c36fbf97e7fc0be91ce1d41569e4c955c731
karancan <karan-91@outlook.com>
2014-04-26 11:43:45 -0400

    Tweak step 3 styling and other changes

--- Setup/views/step3Users.php
+++ Setup/views/step3Users.php
@@ -6,45 +6,60 @@
     <div class="row">
     
         <div class="col-md-12">
-            <h4><i class="fa fa-users"></i> Add users</h4>
+            <h4><i class="fa fa-users"></i> Add an administrative user</h4>
         </div>
         
     </div>
     
     <div class="row">
-        
-        <p class="col-md-12">An administrative user must exist before the application can be used…</p>
-        
+        <p class="col-md-12">At least one administrative user must exist before the application can be used. More users can be added later on…</p>
     </div>
     
-    <form action="index.php?page=go-to-step-4" method="POST" class="row">
-        
-        <div class="form-group col-md-12">
-            <label class="control-label" for="username">Username</label>
-            <input type="text" class="form-control" id="username">
-        </div>
-
-        <div class="form-group col-md-12">
-            <label class="control-label" for="first_name">First name</label>
-            <input type="text" class="form-control" id="first_name">
-        </div>
-
-        <div class="form-group col-md-12">
-            <label class="control-label" for="last_name">Last name</label>
-            <input type="text" class="form-control" id="last_name">
-        </div>
-
-        <div class="form-group col-md-12">
-            <label class="control-label" for="expires_on">Expires on</label>
-            <input type="text" class="form-control datepicker" id="expires_on">
+    <form action="index.php?page=go-to-step-4" method="POST" class="col-md-12">
+            
+        <div class="row">
+            <div class="form-group col-md-4">
+                <label class="control-label" for="username">Username</label>
+                <input type="text" class="form-control" id="username" required>
+            </div>
         </div>
         
-        <p class="col-md-12">More users can be added later on.</p>
+        <div class="row">
+            <div class="form-group col-md-4">
+                <label class="control-label" for="first_name">First name</label>
+                <input type="text" class="form-control" id="first_name" required>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="form-group col-md-4">
+                <label class="control-label" for="last_name">Last name</label>
+                <input type="text" class="form-control" id="last_name" required>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="form-group col-md-4">
+                <label class="control-label" for="expires_on">Expires on</label>
+                <input type="text" class="form-control datepicker" id="expires_on" required>
+            </div>
+        </div>
+            
 
-        <div class="col-md-2">
-            <button type="submit" class="btn btn-success btn-block"><i class="fa fa-check"></i> Save and proceed</button>
+        <div class="row">
+            <div class="col-md-2">
+                <button type="submit" class="btn btn-success"><i class="fa fa-check"></i> Save and proceed</button>
+            </div>
         </div>
         
     </form>
     
 </div>
+<script>
+    
+    //Init datepickers
+    $('.datepicker').datepicker({
+      format: 'yyyy-mm-dd'
+    });
+    
+</script>
