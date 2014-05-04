cea2e6fd5d1739df578641cad830b06f9349c504
karancan <karan-91@outlook.com>
2014-05-04 10:17:53 -0400

    More progress with invoices scripting

--- Admin/includes/js/invoices.js
+++ Admin/includes/js/invoices.js
@@ -9,3 +9,50 @@ $(document).ready(function(){
         }, 1000);
     }
 });
+
+/**
+ *User wants to view invoice items
+ */
+$('[data-action="view-items"]').click(function(){
+    
+    global.highlightRow($(this).closest('tr'));
+    
+    //@todo: ajax
+});
+
+/**
+ *User wants to view invoice files
+ */
+$('[data-action="view-files"]').click(function(){
+    
+    global.highlightRow($(this).closest('tr'));
+    
+    //@todo: ajax
+});
+
+/**
+ *User wants to view invoice notes
+ */
+$('[data-action="view-notes"]').click(function(){
+    
+    global.highlightRow($(this).closest('tr'));
+    
+    //@todo: ajax
+});
+
+/**
+ *User wants to view invoice service provider details
+ */
+$('[data-action="view-sp-details"]').click(function(){
+    
+    global.highlightRow($(this).closest('tr'));
+    
+    //@todo: ajax
+});
+
+/**
+ *One of the following modals has been closed: {view files, view items, view notes, view SP details}
+ */
+$('#admin-invoice-items-modal, #admin-invoice-files-modal, #admin-invoice-notes-modal, #admin-invoice-sp-details-modal').on('hidden.bs.modal', function () {
+    global.removeRowHighlighting($('.invoice-table'));
+});
--- Admin/views/invoicesDrafts.php
+++ Admin/views/invoicesDrafts.php
@@ -52,10 +52,10 @@
                                 echo "<tr data-invoice-id='{$antiXSS->escape($i['invoice_id'], $antiXSS::HTML_ATTR)}'>" .
                                      "<td>" . $i['invoice_id_for_org'] . "</td>" .
                                      "<td>" . $i['client_id'] . "</td>" .
-                                     "<td>" . "<a href='#admin-invoice-sp-details-modal' data-toggle='modal' data-action='view-sp-details'>" . $i['sp_name'] . "</a>" . "</td>" .
-                                     "<td>" . "<a href='#admin-invoice-items-modal' data-toggle='modal' data-action='view-items' class='admin-count-links'>" . $i['item_count'] . "</a>" . "</td>" .
-                                     "<td>" . "<a href='#admin-invoice-files-modal' data-toggle='modal' data-action='view-files' class='admin-count-links'>" . $i['file_count'] . "</a>" . "</td>" .
-                                     "<td>" . "<a href='#admin-invoice-notes-modal' data-toggle='modal' data-action='view-notes' class='admin-count-links'>" . $i['note_count'] . "</a>" . "</td>" .
+                                     "<td>" . "<a href='#admin-invoice-sp-details-modal' data-toggle='modal' data-action='view-sp-details' class='admin-modal-links'>" . $i['sp_name'] . "</a>" . "</td>" .
+                                     "<td>" . "<a href='#admin-invoice-items-modal' data-toggle='modal' data-action='view-items' class='admin-modal-links'>" . $i['item_count'] . "</a>" . "</td>" .
+                                     "<td>" . "<a href='#admin-invoice-files-modal' data-toggle='modal' data-action='view-files' class='admin-modal-links'>" . $i['file_count'] . "</a>" . "</td>" .
+                                     "<td>" . "<a href='#admin-invoice-notes-modal' data-toggle='modal' data-action='view-notes' class='admin-modal-links'>" . $i['note_count'] . "</a>" . "</td>" .
                                      "<td>" . $i['grand_total'] . "</td>" .
                                      "<td>" . ($i['is_approved'] ? 'Yes' : 'No') . "</td>" .
                                      "<td>" . $dateFmt->format($i['inserted_on'], 'date_time') . "</td>" .
