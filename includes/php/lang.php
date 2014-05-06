9bd03b85e6f51c57a88d9e03f438776d28ad0e7c
Vincent <vincent.diep61@gmail.com>
2014-05-05 21:17:04 -0400

    Session language

--- includes/php/lang.php
+++ includes/php/lang.php
@@ -3,14 +3,17 @@
 require 'config.php';
 session_start();
 
+$dbo = new \Interpresense\Includes\DatabaseObject();
+$settings = \Interpresense\Includes\ApplicationSettings::load($dbo);
+
 $acceptableLanguages = unserialize(AVAILABLE_LANGUAGES);
 
 //Do we have a specified language requested and is it a valid language?
 if (isset($_GET['lang']) && in_array($_GET['lang'], $acceptableLanguages)){
     $_SESSION['lang'] = $_GET['lang'];
 } else {
-    //@todo: fetch default language from database and use that
+    $_SESSION['lang'] = $settings['institution_default_lang'];
 }
 
-header("Location: " . $_SERVER['HTTP_REFERER']); //@todo: what is HTTP referer is empty?
+header("Location: " . $_SERVER['HTTP_REFERER']); //@todo: what if HTTP referer is empty?
 exit;
