55f3095d428a36948c869832a4f75788a0c9e3bc
Vincent <vincent.diep61@gmail.com>
2014-05-09 15:00:45 -0400

    Encapsulate

--- ServiceProvider/includes/js/invoice.js
+++ ServiceProvider/includes/js/invoice.js
@@ -1,158 +1,160 @@
-/**
- * Change the styling of an input to a negative state
- * @param {$} element An input element
- */
-function showInputFailure(element) {
-    element.parent().removeClass('has-success').addClass('has-error');
-}
-
-/**
- * Change the styling of an input to a positive state
- * @param {$} element An input element
- */
-function showInputSuccess(element) {
-    element.parent().removeClass('has-error').addClass('has-success');
-}
-
-/**
- * Calculates the amount based on hours and rate
- * @param {$} $row The input item row
- */
-function calculateAmount($row) {
-    var hours, amount;
-            
-    hours = (parseInt($('.invoice-item-hours', $row).text(), 10) + parseInt($('.invoice-item-minutes', $row).text(), 10)/60),
-    amount = (hours * parseInt($('.invoice-item-rate', $row).val(), 10)).toFixed(2);
-    
-    if(isNaN(amount)) {
-        amount = '0.00';
+(function(){
+    /**
+     * Change the styling of an input to a negative state
+     * @param {$} element An input element
+     */
+    function showInputFailure(element) {
+        element.parent().removeClass('has-success').addClass('has-error');
     }
-    
-    $('.invoice-item-amounts', $row).text(amount);
-    
-    totalAmounts();
-}
 
-/**
- * Calculates the total amount of the invoice
- */
-function totalAmounts() {
-    //Update the total of all invoice items
-    var total_amount = $('.invoice-item-amounts').toArray().reduce(function(p, v) {
-        return p += Number(v.textContent);
-    }, 0);
-
-    $('#invoice-total-dollar-amount').text('$' + total_amount.toFixed(2));
-}
-
-$('.invoice-item-date').datepicker({
-    format: 'yyyy-mm-dd'
-});
-
-$('.invoice-item-start-time, .invoice-item-end-time').change(function(){
-    var $thisRow = $(this).closest(".invoice-item-row"),
-        date = $('.invoice-item-date', $thisRow).val() || '2014-05-01',
-        startTime = new Date(date + 'T' + $('.invoice-item-start-time', $thisRow).val()),
-        endTime = new Date(date + 'T' + $('.invoice-item-end-time', $thisRow).val()),
-        diffInMinutes = (endTime.getTime() - startTime.getTime()) / 1000 / 60;
-    
-    // Verify validity
-    if(startTime > endTime) {
-        showInputFailure($('.invoice-item-start-time', $thisRow));
-        showInputFailure($('.invoice-item-end-time', $thisRow));
-    } else {
-        showInputSuccess($('.invoice-item-start-time', $thisRow));
-        showInputSuccess($('.invoice-item-end-time', $thisRow));
+    /**
+     * Change the styling of an input to a positive state
+     * @param {$} element An input element
+     */
+    function showInputSuccess(element) {
+        element.parent().removeClass('has-error').addClass('has-success');
     }
-    
-    // Update hours
-    $('.invoice-item-hours', $thisRow).text(Math[(diffInMinutes / 60) < 0 ? 'ceil' : 'floor'](diffInMinutes / 60));
-    $('.invoice-item-minutes', $thisRow).text(diffInMinutes % 60);
-    
-    // Calculate amount
-    calculateAmount($thisRow);
-});
 
-$('.invoice-item-rate').change(function(){
-    'use strict';
-    var $thisRow = $(this).closest(".invoice-item-row");
-    if(this.checkValidity()) {
-        calculateAmount($thisRow);
-    }
-});
+    /**
+     * Calculates the amount based on hours and rate
+     * @param {$} $row The input item row
+     */
+    function calculateAmount($row) {
+        var hours, amount;
 
-// Invoke tooltip popovers
-$("[data-popover='true']").popover({
-    container: 'body',
-    trigger: 'hover'
-});
+        hours = (parseInt($('.invoice-item-hours', $row).text(), 10) + parseInt($('.invoice-item-minutes', $row).text(), 10)/60),
+        amount = (hours * parseInt($('.invoice-item-rate', $row).val(), 10)).toFixed(2);
 
-/**
- *User wants to clear the form
- */
-$('#invoice-btn-clear').click(function(){
-    if(confirm("Are you sure you want to clear this form?")){
-        window.location.reload(true);
-    }
-});
-
-/**
- *User wants to save a draft
- */
-$('#invoice-btn-draft').click(function(){
-    $('#mode').val('draft');
-    alert("Draft will be saved...you will receive an email");
-});
-
-/**
- *User wants to submit the invoice
- */
-$('#invoice_form').submit(function(){
-    $('#mode').val('final');
-    
-    if (this.checkValidity()) {
-        alert("Invoice will be submitted...you will receive an email");
-    }
-});
-
-/**
- * User is entering information into the form
- */
-$('input, select', '#invoice_form').change(function() {
-    'use strict';
-    
-    //Update the fields' UI state
-    if(!this.checkValidity()) {
-        showInputFailure($(this));
-    } else {
-        showInputSuccess($(this));
-    }
-});
-
-/**
- *User is entering information in to the input fields in invoice rows
- */
-$(document).on('input focusout', '.invoice-item-input', function() {
-    'use strict';
-    var $thisRow = $(this).closest(".invoice-item-row");
-
-    if (!$thisRow.next("tr").length) {
-        //Variable determining if all the fields in a row are succesfully completed.
-        //Check to see if no fields in the row are incomplete or erroneous
-        var all_complete = !$thisRow.find('.invoice-item-input').filter(function(){
-            return $(this).hasClass("has-error") || !this.checkValidity();
-        }).length;
-
-        //If all fields have been completed, we can add a new row
-        if (all_complete) {
-            var $clone = $thisRow.clone();
-            $clone.find('.invoice-item-input').val('');
-            $clone.find('.has-success').each(function() {
-                $(this).removeClass("has-success");
-            });
-            $thisRow.after($clone);
+        if(isNaN(amount)) {
+            amount = '0.00';
         }
 
+        $('.invoice-item-amounts', $row).text(amount);
+
+        totalAmounts();
     }
 
-});
+    /**
+     * Calculates the total amount of the invoice
+     */
+    function totalAmounts() {
+        //Update the total of all invoice items
+        var total_amount = $('.invoice-item-amounts').toArray().reduce(function(p, v) {
+            return p += Number(v.textContent);
+        }, 0);
+
+        $('#invoice-total-dollar-amount').text('$' + total_amount.toFixed(2));
+    }
+
+    $('.invoice-item-date').datepicker({
+        format: 'yyyy-mm-dd'
+    });
+
+    $('.invoice-item-start-time, .invoice-item-end-time').change(function(){
+        var $thisRow = $(this).closest(".invoice-item-row"),
+            date = $('.invoice-item-date', $thisRow).val() || '2014-05-01',
+            startTime = new Date(date + 'T' + $('.invoice-item-start-time', $thisRow).val()),
+            endTime = new Date(date + 'T' + $('.invoice-item-end-time', $thisRow).val()),
+            diffInMinutes = (endTime.getTime() - startTime.getTime()) / 1000 / 60;
+
+        // Verify validity
+        if(startTime > endTime) {
+            showInputFailure($('.invoice-item-start-time', $thisRow));
+            showInputFailure($('.invoice-item-end-time', $thisRow));
+        } else {
+            showInputSuccess($('.invoice-item-start-time', $thisRow));
+            showInputSuccess($('.invoice-item-end-time', $thisRow));
+        }
+
+        // Update hours
+        $('.invoice-item-hours', $thisRow).text(Math[(diffInMinutes / 60) < 0 ? 'ceil' : 'floor'](diffInMinutes / 60));
+        $('.invoice-item-minutes', $thisRow).text(diffInMinutes % 60);
+
+        // Calculate amount
+        calculateAmount($thisRow);
+    });
+
+    $('.invoice-item-rate').change(function(){
+        'use strict';
+        var $thisRow = $(this).closest(".invoice-item-row");
+        if(this.checkValidity()) {
+            calculateAmount($thisRow);
+        }
+    });
+
+    // Invoke tooltip popovers
+    $("[data-popover='true']").popover({
+        container: 'body',
+        trigger: 'hover'
+    });
+
+    /**
+     *User wants to clear the form
+     */
+    $('#invoice-btn-clear').click(function(){
+        if(confirm("Are you sure you want to clear this form?")){
+            window.location.reload(true);
+        }
+    });
+
+    /**
+     *User wants to save a draft
+     */
+    $('#invoice-btn-draft').click(function(){
+        $('#mode').val('draft');
+        alert("Draft will be saved...you will receive an email");
+    });
+
+    /**
+     *User wants to submit the invoice
+     */
+    $('#invoice_form').submit(function(){
+        $('#mode').val('final');
+
+        if (this.checkValidity()) {
+            alert("Invoice will be submitted...you will receive an email");
+        }
+    });
+
+    /**
+     * User is entering information into the form
+     */
+    $('input, select', '#invoice_form').change(function() {
+        'use strict';
+
+        //Update the fields' UI state
+        if(!this.checkValidity()) {
+            showInputFailure($(this));
+        } else {
+            showInputSuccess($(this));
+        }
+    });
+
+    /**
+     *User is entering information in to the input fields in invoice rows
+     */
+    $(document).on('input focusout', '.invoice-item-input', function() {
+        'use strict';
+        var $thisRow = $(this).closest(".invoice-item-row");
+
+        if (!$thisRow.next("tr").length) {
+            //Variable determining if all the fields in a row are succesfully completed.
+            //Check to see if no fields in the row are incomplete or erroneous
+            var all_complete = !$thisRow.find('.invoice-item-input').filter(function(){
+                return $(this).hasClass("has-error") || !this.checkValidity();
+            }).length;
+
+            //If all fields have been completed, we can add a new row
+            if (all_complete) {
+                var $clone = $thisRow.clone();
+                $clone.find('.invoice-item-input').val('');
+                $clone.find('.has-success').each(function() {
+                    $(this).removeClass("has-success");
+                });
+                $thisRow.after($clone);
+            }
+
+        }
+
+    });
+}());
