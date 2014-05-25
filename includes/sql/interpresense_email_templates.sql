73bcfb76c3ff1cb8926148fef9541d886df1fcd9
karancan <karan-91@outlook.com>
2014-05-25 10:52:59 -0400

    More emails added

--- includes/sql/interpresense_email_templates.sql
+++ includes/sql/interpresense_email_templates.sql
@@ -17,7 +17,10 @@ CREATE TABLE `interpresense_email_templates` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
--- @todo: add insert statements for emails that are available (invoice added, invoice approved, account creation, account update account password reset, ...?)
-INSERT INTO `interpresense_email_templates` (`email_id`, `name`, `content`, `description`, `subject`) VALUES (1, 'Account creation', 'A fancy email :)', 'This email is sent out when an administrative user creates an account.', 'Interpresense - Account created');
-INSERT INTO `interpresense_email_templates` (`email_id`, `name`, `content`, `description`, `subject`) VALUES (2, 'Account modification', 'A fancy email :)', 'This email is sent out when an administrative user changes user details for an account.', 'Interpresense - Account details changed');
-INSERT INTO `interpresense_email_templates` (`email_id`, `name`, `content`, `description`, `subject`) VALUES (3, 'Password reset', '<p>A fancy email :)</p><p>#passwordResetLink</p>', 'This email is sent out when an user requests a password reset.', 'Interpresense - Password reset');
+-- @todo: add sensible content for all emails
+INSERT INTO `interpresense_email_templates` (`email_id`, `name`, `content`, `description`, `subject`) VALUES (1, 'Account creation', 'A fancy email :)', 'This email is sent out when an administrative user creates an account', 'Interpresense - Account created');
+INSERT INTO `interpresense_email_templates` (`email_id`, `name`, `content`, `description`, `subject`) VALUES (2, 'Account modification', 'A fancy email :)', 'This email is sent out when an administrative user changes user details for an account', 'Interpresense - Account details changed');
+INSERT INTO `interpresense_email_templates` (`email_id`, `name`, `content`, `description`, `subject`) VALUES (3, 'Account password reset', '<p>A fancy email :)</p><p>#passwordResetLink</p>', 'This email is sent out when an user requests a password reset', 'Interpresense - Password reset');
+INSERT INTO `interpresense_email_templates` (`email_id`, `name`, `content`, `description`, `subject`) VALUES (4, 'Invoice added', '<p>Content</p>', 'This email is sent out when a service provider adds an invoice', 'Interpresense - Invoice added');
+INSERT INTO `interpresense_email_templates` (`email_id`, `name`, `content`, `description`, `subject`) VALUES (5, 'Invoice approved', '<p>Content</p>', 'This email is sent out when an admin user approves an invoice', 'Interpresense - Invoice approved');
+INSERT INTO `interpresense_email_templates` (`email_id`, `name`, `content`, `description`, `subject`) VALUES (6, 'Invoice marked as draft', '<p>Content</p>', 'This email is sent out when an admin user marks an invoice as a draft', 'Interpresense - Invoice marked as draft');
