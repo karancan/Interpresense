150a26951fd7006e9fef7e9ea47e1311b41b722a
Vincent <vincent.diep61@gmail.com>
2014-06-10 02:39:39 -0400

    Internationalization of login page

--- Admin/views/login.php
+++ Admin/views/login.php
@@ -7,7 +7,7 @@ if ($_GET['mode'] === 'login-failed'){
 ?>
 <div class="col-md-4 col-md-offset-4 alert alert-danger alert-dismissable">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-    <strong>Unsuccessful!</strong> That user name and password combination is invalid…
+    <strong><?php $translate->_e('fail'); ?></strong> <?php $translate->_e('errorFail'); ?>
 </div>
 <?php } ?>
 
@@ -16,14 +16,14 @@ if ($_GET['mode'] === 'unconfirmed-user'){
 ?>
 <div class="col-md-4 col-md-offset-4 alert alert-warning alert-dismissable">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-    <strong>Unsuccessful!</strong> Your account has not been activated yet…
+    <strong><?php $translate->_e('fail'); ?></strong> <?php $translate->_e('errorUnconfirmed'); ?>
 </div>
 
 <?php } elseif ($_GET['mode'] === 'expired-user') { ?>
 
 <div class="col-md-4 col-md-offset-4 alert alert-warning alert-dismissable">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-    <strong>Unsuccessful!</strong> Your account has been expired…
+    <strong><?php $translate->_e('fail'); ?></strong> <?php $translate->_e('errorExpired'); ?>
 </div>
 <?php } ?>
 
@@ -36,20 +36,21 @@ if ($_GET['mode'] === 'unconfirmed-user'){
             <div class="col-md-6 col-md-offset-3 admin-login-form-inputs">
                 
                 <div class="row">
-                    <h4><i class="fa fa-lock"></i> &nbsp;Please log in to continue…</h4>
+                    <h4><i class="fa fa-lock"></i> &nbsp;<?php $translate->_e('title'); ?></h4>
                 </div>
                 
                 <div class="row">
                     <div class="input-group">
-                        <input id="admin_user_name" name="user_name" type="text" class="form-control" placeholder="Username" required>
+                        <label for="admin_user_name" class="sr-only"><?php $translate->_e('usernamePlaceholder'); ?></label>
+                        <input id="admin_user_name" name="user_name" type="text" class="form-control" placeholder="<?php echo $antiXSS->escape($translate->__('usernamePlaceholder'), $antiXSS::HTML_ATTR) ?>" required>
                         <div class="input-group-btn">
-                            <button type="button" class="btn btn-info" tabindex="-1">Options</button>
+                            <button type="button" class="btn btn-info" tabindex="-1"><?php $translate->_e('optionsBtn'); ?></button>
                             <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" tabindex="-1">
                                 <span class="caret"></span>
                             </button>
                             <ul class="dropdown-menu">
-                                <li><a href="mailto:<?= EMAIL_ALIAS_INTERPRESENSE_COORDINATOR . EMAIL_ORG_STAFF_DOMAIN ?>?subject=Interpresense:%20New%20account%20request"><i class="fa fa-plus"></i> &nbsp; Add new user</a></li>
-                                <li><a href="mailto:<?= EMAIL_ALIAS_INTERPRESENSE_COORDINATOR . EMAIL_ORG_STAFF_DOMAIN ?>?subject=Interpresense:%20Forgot%20username"><i class="fa fa-refresh"></i> &nbsp; Forgot username</a></li>
+                                <li><a href="mailto:<?= EMAIL_ALIAS_INTERPRESENSE_COORDINATOR . EMAIL_ORG_STAFF_DOMAIN ?>?subject=Interpresense:%20New%20account%20request"><i class="fa fa-plus"></i> &nbsp; <?php $translate->_e('addNewUserItem'); ?></a></li>
+                                <li><a href="mailto:<?= EMAIL_ALIAS_INTERPRESENSE_COORDINATOR . EMAIL_ORG_STAFF_DOMAIN ?>?subject=Interpresense:%20Forgot%20username"><i class="fa fa-refresh"></i> &nbsp; <?php $translate->_e('forgotUsernameItem'); ?></a></li>
                             </ul>
                         </div>
                     </div>
@@ -59,14 +60,15 @@ if ($_GET['mode'] === 'unconfirmed-user'){
                 
                 <div class="row">
                     <div class="input-group">
-                        <input id="admin_password" name="user_password" type="password" class="form-control" placeholder="Password" required>
+                        <label class="sr-only" for="admin_password"><?php $translate->_e('passwordPlaceholder'); ?></label>
+                        <input id="admin_password" name="user_password" type="password" class="form-control" placeholder="<?php echo $antiXSS->escape($translate->__('passwordPlaceholder'), $antiXSS::HTML_ATTR); ?>" required>
                         <div class="input-group-btn">
-                            <button type="button" class="btn btn-info" tabindex="-1">Options</button>
+                            <button type="button" class="btn btn-info" tabindex="-1"><?php $translate->_e('optionsBtn'); ?></button>
                             <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" tabindex="-1">
                                 <span class="caret"></span>
                             </button>
                             <ul class="dropdown-menu">
-                                <li><a href="index.php?page=reset-password"><i class="fa fa-refresh"></i> &nbsp; Forgot password</a></li>
+                                <li><a href="index.php?page=reset-password"><i class="fa fa-refresh"></i> &nbsp; <?php $translate->_e('forgotPasswordItem'); ?></a></li>
                             </ul>
                         </div>
                     </div>
@@ -82,7 +84,7 @@ if ($_GET['mode'] === 'unconfirmed-user'){
                 
                 <div class="row">
                 
-                    <button type="submit" class="btn btn-success"><i class="fa fa-check"></i> Log me in</button>
+                    <button type="submit" class="btn btn-success"><i class="fa fa-check"></i> <?php $translate->_e('submitBtn'); ?></button>
                 
                 </div>
                 
